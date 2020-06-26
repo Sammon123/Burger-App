95dbdb94d3d627cdc9d4c330003604de48110dbf
Peter Sammon <petersammon2@gmail.com>
2020-06-26 16:22:06 -0400

    set up mysql database

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Cheeseburger Deluxe');
+INSERT INTO burgers (burger_name) VALUES ('Big Mac');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Daves Double', true);
