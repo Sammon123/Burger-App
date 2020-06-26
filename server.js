9fdf048f3f9e6c0a7514b1785eb9694c9f1f8688
Peter Sammon <petersammon2@gmail.com>
2020-06-26 16:04:51 -0400

    set up express

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-const express = require('express');
+const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
+})
